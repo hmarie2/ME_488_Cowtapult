f2e76c11f85dee0f73667e3050d9f048fda59bf6
hmarie2 <hmarie@pdx.edu>
2017-12-02 11:13:15 -0800

    done working on the r files and excel files for the morning

--- Analysis.R
+++ Analysis.R
@@ -1,3 +1,17 @@
 ## Data eval
 
+# read in the data
 results <- read.csv("data_csv.csv")
+
+# make the factors recognizable to R
+results$A <- factor(results$A)
+results$B <- factor(results$B)
+results$C <- factor(results$C)
+results$D <- factor(results$D)
+
+# run ANOVA tests
+DistAOV <- aov(YLD~A+B+C+D, data=results)
+anova(DistAOV)
+
+DistLM <- lm(YLD~A+B+C+D, data=results)
+summary(DistLM)
--- Initial.R
+++ Initial.R
@@ -3,3 +3,5 @@
 library(FrF2)
 
 FrF2(nfactors = 4, resolution = 4,replications = 2)
+#design.info((FrF2(nruns=16, nfactors=4,)))$aliased
+
